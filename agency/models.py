0c69868ef639934326224026cc1d2fb01a08f790
Pasha <pavik.varganov@gmail.com>
2024-12-02 16:44:03 +0200

    create agency models

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Cat(AbstractUser):
+    name = models.CharField(max_length=63)
+    experience = models.IntegerField(default=0)
+    breed = models.CharField(max_length=63)
+    salary = models.IntegerField()
+
+
+class Target(models.Model):
+    name = models.CharField(max_length=63)
+    country = models.CharField(max_length=63)
+    notes = models.TextField()
+    complete = models.BooleanField(default=False)
+
+
+class Mission(models.Model):
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    target = models.ManyToManyField(Target)
+    complete = models.BooleanField(default=False)
