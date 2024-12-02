f519044d544813ae2f0afe4a53e04054462ae39d
Pasha <pavik.varganov@gmail.com>
2024-12-02 18:37:06 +0200

    create views

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from agency.models import Cat, Target, Mission
+from agency.serializers import (
+    CatSerializer,
+    TargetSerializer,
+    MissionSerializer
+)
+
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class TargetViewSet(viewsets.ModelViewSet):
+    queryset = Target.objects.all()
+    serializer_class = TargetSerializer
+
+
+class MissionViewSet(viewsets.ModelViewSet):
+    queryset = Mission.objects.all()
+    serializer_class = MissionSerializer
